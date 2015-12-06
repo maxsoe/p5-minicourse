92fa988d32c4cdbbad2888412738254a8e97d94c
Max Soe <maxsoe@gmail.com>
2015-12-06 19:49:19 +0000

    Wikipedia API

--- js/script.js
+++ js/script.js
@@ -41,7 +41,25 @@ function loadData() {
       $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
 
-    
+    // Wikipedia AJAX request
+    var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr +
+    '&format=json&callback=wikiCallback';
+
+    $.ajax({
+      url: wikiURL,
+      dataType: "jsonp",
+      // jsonp: "callback",
+      success: function(response) {
+        var articleList = response[1];
+
+        for (var i =0; i < articleList.length; i++) {
+          articleStr = articleList[i];
+          var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+          $wikiElem.append('<li><a href="' + url + '">' +
+            articleStr + '</a></li>');
+        };
+      }
+    });
 
     return false;
 };
