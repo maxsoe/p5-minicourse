c7fbd8e3c8364879a48a124006e72fee2639b6b1
Max Soe <maxsoe@gmail.com>
2015-12-06 19:36:33 +0000

    NY Times API

--- js/script.js
+++ js/script.js
@@ -22,6 +22,26 @@ function loadData() {
     $body.append('<img class="bgimg" src="' + streetviewURL + '">');
 
     // NY Times Ajax request
+    var mynytimeskey = '5e4af54df9115fd5451b40563f2a94b7:11:73680588';
+    var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=' + mynytimeskey;
+
+    $.getJSON(nytimesURL, function(data) {
+      $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+      articles = data.response.docs;
+      for (var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        console.log("Current article says " + article.web_url);
+        $nytElem.append('<li class="article">' +
+          '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +
+          '<p>' + article.snippet + '</p>' +
+        '</li>');
+      };
+
+    }).fail(function(e){
+      $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+
+    
 
     return false;
 };
